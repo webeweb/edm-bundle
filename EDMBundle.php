c31a3fbcc57c288f3ac956971b009cfe73fb3ba7
webeweb <support@webeweb.fr>
2017-12-15 15:36:49 +0100

    Add main bundle class

---
+++ EDMBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * EDM bundle.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle
+ * @final
+ */
+final class EDMBundle extends Bundle {
+
+}
