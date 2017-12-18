12badbab2a43c47adbeab2981c8f72e51b15aa59
webeweb <support@webeweb.fr>
2017-12-18 15:47:07 +0100

    Add Document interface

---
+++ Entity/DocumentInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Entity;
+
+/**
+ * Document interface.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Entity
+ */
+interface DocumentInterface {
+
+	/**
+	 * Type "Directory".
+	 *
+	 * @var Integer
+	 */
+	const TYPE_DIRECTORY = 117;
+
+	/**
+	 * Type "Document".
+	 *
+	 * @var Integer
+	 */
+	const TYPE_DOCUMENT = 95;
+
+}
