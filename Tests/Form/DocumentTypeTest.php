13fa50362768fe8158d51a7b1b2d604450853ef2
webeweb <support@webeweb.fr>
2017-12-18 15:55:00 +0100

    Add Document type

---
+++ Tests/Form/DocumentTypeTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests\Form;
+
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WBW\Bundle\EDMBundle\Form\DocumentType;
+
+/**
+ * Document type test.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Test\Form
+ * @final
+ */
+final class DocumentTypeTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Tests the buildForm() method.
+	 *
+	 * @return void
+	 */
+	public function testBuildForm() {
+
+		// Set the mocks.
+		$mockBuilder = $this->getMockBuilder(FormBuilderInterface::class)->getMock();
+		$mockBuilder->expects($this->any())->method("add")->willReturnCallback(function($child, $type = null, array $options = array()) use($mockBuilder) {
+			return $mockBuilder;
+		});
+
+		$obj = new DocumentType();
+
+		$obj->buildForm($mockBuilder, []);
+	}
+
+	/**
+	 * Tests the configureOptions() method.
+	 *
+	 * @return void
+	 */
+	public function testConfigureOptions() {
+
+		$obj = new DocumentType();
+		$arg = new OptionsResolver();
+
+		$obj->configureOptions($arg);
+		$this->assertEquals(true, $arg->hasDefault("data_class"));
+	}
+
+	/**
+	 * Tests the getBlockPrefix() method.
+	 *
+	 * @return void
+	 */
+	public function testGetBlockPrefix() {
+
+		$obj = new DocumentType();
+
+		$this->assertEquals("edmbundle_document", $obj->getBlockPrefix());
+	}
+
+}
