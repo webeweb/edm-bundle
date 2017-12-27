2caa850fcaddd224f9ba004092e7cf008eb2f238
webeweb <support@webeweb.fr>
2017-12-27 18:01:49 +0100

    Add Document move type

---
+++ Form/Type/DocumentMoveType.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Form\Type;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WBW\Bundle\EDMBundle\Entity\Document;
+use WBW\Library\Core\Form\Factory\FormFactory;
+use WBW\Library\Core\Sort\Tree\Alphabetical\AlphabeticalTreeSort;
+
+/**
+ * Document move type.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Form\Type
+ * @final
+ */
+final class DocumentMoveType extends AbstractDocumentType {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$parent = FormFactory::createEntityType(Document::class, AlphabeticalTreeSort::sort(array_values($options["entity.parent"])), ["empty" => true]);
+		$builder
+			->add("parent", EntityType::class, array_merge(["label" => "label.parent", "required" => false], $parent))
+			->addEventListener(FormEvents::PRE_SET_DATA, [$this, "preSetData"]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults([
+			"data_class"		 => Document::class,
+			"translation_domain" => "EDMBundle",
+		]);
+		$resolver->setRequired("entity.parent");
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getBlockPrefix() {
+		return "edmbundle_document";
+	}
+
+}
