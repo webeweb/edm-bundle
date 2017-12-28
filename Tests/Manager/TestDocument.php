dc52c07c30a445500c66109cf543a7fb66cbc537
webeweb <support@webeweb.fr>
2017-12-28 12:18:17 +0100

    Create a Test document entity

---
+++ Tests/Manager/TestDocument.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests\Manager;
+
+use WBW\Bundle\EDMBundle\Entity\Document;
+
+/**
+ * Test document entity.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Manager
+ * @final
+ */
+final class TestDocument extends Document {
+
+	/**
+	 * Set the id.
+	 *
+	 * @param integer $id The id.
+	 */
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+}
