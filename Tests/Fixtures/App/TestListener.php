eb003fbfd0f12de54b4c37056a07f275eaeb50ee
webeweb <support@webeweb.fr>
2018-01-02 17:48:47 +0100

    Add into functionnal tests the not implemented events

---
+++ Tests/Fixtures/App/TestListener.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests\Fixtures\App;
+
+use WBW\Bundle\EDMBundle\Event\DocumentEvent;
+
+/**
+ * Test listener.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Tests\Fixtures\App
+ * @final
+ */
+final class TestListener {
+
+	/**
+	 * On edited document.
+	 *
+	 * @param DocumentEvent $event The event.
+	 * @return void
+	 */
+	public function onEditedDocument(DocumentEvent $event) {
+		// NOTHING TO DO.
+	}
+
+	/**
+	 * On opened directory.
+	 *
+	 * @param DocumentEvent $event The event.
+	 * @return void
+	 */
+	public function onOpenedDocument(DocumentEvent $event) {
+		// NOTHING TO DO.
+	}
+
+}
