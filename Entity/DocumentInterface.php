b51bc20ba6d5a5212bfcf3e24e6c684f0d12529b
webeweb <support@webeweb.fr>
2018-02-15 14:33:19 +0100

    Add DocumentInterface

--- Entity/DocumentInterface.php
+++ Entity/DocumentInterface.php
@@ -11,6 +11,9 @@
 
 namespace WBW\Bundle\EDMBundle\Entity;
 
+use DateTime;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 /**
  * Document interface.
  *
@@ -33,4 +36,138 @@ interface DocumentInterface {
      */
     const TYPE_DOCUMENT = 95;
 
+    /**
+     * Decrease the size.
+     *
+     * @param integer $size The size.
+     * @return DocumentInterface Returns the document.
+     */
+    public function decreaseSize($size);
+
+    /**
+     * Get the childrens.
+     *
+     * @return DocumentInterface[] Returns the childrens.
+     */
+    public function getChildrens();
+
+    /**
+     * Get the created at.
+     *
+     * @return DateTime Returns the created at.
+     */
+    public function getCreatedAt();
+
+    /**
+     * Get the extension.
+     *
+     * @return string Returns the extension.
+     */
+    public function getExtension();
+
+    /**
+     * Get the id.
+     *
+     * @return integer Returns the id.
+     */
+    public function getId();
+
+    /**
+     * Get the mime type.
+     *
+     * @return string Returns the mime type.
+     */
+    public function getMimeType();
+
+    /**
+     * Get the name.
+     *
+     * @return string Returns the name.
+     */
+    public function getName();
+
+    /**
+     * Get the number of downloads.
+     *
+     * @return string Returns the number of downloads.
+     */
+    public function getNumberDownloads();
+
+    /**
+     * Get the parent.
+     *
+     * @return DocumentInterface Returns the parent.
+     */
+    public function getParent();
+
+    /**
+     * Get the parent backed up.
+     *
+     * @return DocumentInterface Returns the parent backed up.
+     */
+    public function getParentBackedUp();
+
+    /**
+     * Get the size.
+     *
+     * @return integer Returns the size.
+     */
+    public function getSize();
+
+    /**
+     * Get the type.
+     *
+     * @return integer Returns the type.
+     */
+    public function getType();
+
+    /**
+     * Get the updated at.
+     *
+     * @return DateTime Returns the updated at.
+     */
+    public function getUpdatedAt();
+
+    /**
+     * Get the upload.
+     *
+     * @return UploadedFile Returns the upload file.
+     */
+    public function getUpload();
+
+    /**
+     * Determines if the document has childrens.
+     *
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    public function hasChildrens();
+
+    /**
+     * Increments the number of downloads.
+     *
+     * @return DocumentInterface Returns the document.
+     */
+    public function incrementNumberDownloads();
+
+    /**
+     * Increase the size.
+     *
+     * @param integer $size The size.
+     * @return DocumentInterface Returns the document.
+     */
+    public function increaseSize($size);
+
+    /**
+     * Determines if the document is a directory.
+     *
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    public function isDirectory();
+
+    /**
+     * Determines if the document is a document.
+     *
+     * @return boolean Returns true in case of success, false otherwise.
+     */
+    public function isDocument();
 }
