1f15f2c913a442ac687abd4b42241c26d5c3fdfa
webeweb <support@webeweb.fr>
2018-04-24 10:41:17 +0200

    Remove final keyword

--- Event/DocumentEvent.php
+++ Event/DocumentEvent.php
@@ -19,9 +19,8 @@ use WBW\Bundle\EDMBundle\Entity\DocumentInterface;
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\EDMBundle\Event
- * @final
  */
-final class DocumentEvent extends Event {
+class DocumentEvent extends Event {
 
     /**
      * Document.
