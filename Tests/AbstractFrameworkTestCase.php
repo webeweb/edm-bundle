167c7e49e8fc15ed8750885b1069f91700e9c8d8
webeweb <support@webeweb.fr>
2018-10-04 09:19:05 +0200

    Add abstract EDM framework text case

---
+++ Tests/AbstractFrameworkTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2018 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests;
+
+use WBW\Bundle\BootstrapBundle\Tests\Cases\AbstractBootstrapFrameworkTestCase;
+
+/**
+ * Abstract EDM framework test case.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Tests
+ * @abstract
+ */
+abstract class AbstractFrameworkTestCase extends AbstractBootstrapFrameworkTestCase {
+
+}
