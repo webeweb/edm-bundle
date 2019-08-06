4258cc212a7ccefad8b843aa8406660d6d91bc46
webeweb <support@webeweb.fr>
2019-08-06 13:07:38 +0200

    Zip Dropzone 5.3.0 assets

---
+++ Tests/Helper/AssetsHelperTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests\Helper;
+
+use WBW\Bundle\CoreBundle\Tests\Fixtures\Helper\TestAssetsHelper;
+use WBW\Bundle\EDMBundle\Tests\AbstractTestCase;
+
+/**
+ * Assets helper test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Tests\Helper
+ */
+class AssetsHelperTest extends AbstractTestCase {
+
+    /**
+     * Directory "assets".
+     *
+     * @var string
+     */
+    private $directoryAssets;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // Set the directories.
+        $this->directoryAssets = getcwd() . "/Resources/assets";
+    }
+
+    /**
+     * Tests the listAssets() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testListAssets() {
+
+        $res = TestAssetsHelper::listAssets($this->directoryAssets);
+        $this->assertCount(1, $res);
+
+        $this->assertRegexp("/" . preg_quote("dropzone-5.3.0.zip") . "$/", $res[0]);
+    }
+}
