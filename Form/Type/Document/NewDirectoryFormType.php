971cf75804d6abde108bace252ff7c67ce28178d
webeweb <support@webeweb.fr>
2019-10-24 20:55:36 +0200

    Update new Directory form type

--- Form/Type/Document/NewDirectoryFormType.php
+++ Form/Type/Document/NewDirectoryFormType.php
@@ -14,22 +14,27 @@ namespace WBW\Bundle\EDMBundle\Form\Type\Document;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use WBW\Bundle\EDMBundle\DependencyInjection\WBWEDMExtension;
 use WBW\Bundle\EDMBundle\Entity\Document;
+use WBW\Bundle\EDMBundle\Form\Type\AbstractDocumentFormType;
 
 /**
- * New document type.
+ * New directory form type.
  *
  * @author webeweb <https://github.com/webeweb/>
  * @package WBW\Bundle\EDMBundle\Form\Type\Document
  */
-class NewDocumentType extends AbstractDocumentType {
+class NewDirectoryFormType extends AbstractDocumentFormType {
 
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
+
+        $disabled = $options["disabled"];
+
         $builder
-            ->add("name", TextType::class, ["label" => "label.name", "required" => false]);
+            ->add("name", TextType::class, ["label" => "label.name", "disabled" => $disabled, "required" => false]);
     }
 
     /**
@@ -38,7 +43,7 @@ class NewDocumentType extends AbstractDocumentType {
     public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults([
             "data_class"         => Document::class,
-            "translation_domain" => "EDMBundle",
+            "translation_domain" => "WBWEDMBundle",
         ]);
     }
 
@@ -46,7 +51,6 @@ class NewDocumentType extends AbstractDocumentType {
      * {@inheritdoc}
      */
     public function getBlockPrefix() {
-        return "edmbundle_new_document";
+        return WBWEDMExtension::EXTENSION_ALIAS . "_new_directory";
     }
-
 }
