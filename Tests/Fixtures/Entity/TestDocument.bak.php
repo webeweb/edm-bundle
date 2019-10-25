8f28a9f2113530026b7e1858297bba9c5dfa1bd5
webeweb <support@webeweb.fr>
2019-10-25 19:27:42 +0200

    Add missing file

---
+++ Tests/Fixtures/Entity/TestDocument.bak.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2017 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests\Fixtures\Entity;
+
+use WBW\Bundle\EDMBundle\Entity\Document;
+use WBW\Bundle\EDMBundle\Model\DocumentInterface;
+
+/**
+ * Test document entity.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Tests\Fixtures\Entity
+ */
+class TestDocument extends Document {
+
+    /**
+     * Set the id.
+     *
+     * @param int $id The id.
+     * @return DocumentInterface Returns this document.
+     */
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+}
