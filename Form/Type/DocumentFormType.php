36ff9c8c4b715d96b655141b15380a0a54bdf777
webeweb <support@webeweb.fr>
2019-10-27 15:12:40 +0100

    Add Document form type

---
+++ Form/Type/DocumentFormType.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WBW\Bundle\EDMBundle\DependencyInjection\WBWEDMExtension;
+use WBW\Bundle\EDMBundle\Entity\Document;
+
+/**
+ * Document form type.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Form\Type
+ */
+class DocumentFormType extends AbstractDocumentFormType {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
+        $disabled = $options["disabled"];
+
+        $builder
+            ->add("name", TextType::class, ["label" => "label.name", "disabled" => $disabled, "required" => false]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            "data_class"         => Document::class,
+            "translation_domain" => "WBWEDMBundle",
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix() {
+        return WBWEDMExtension::EXTENSION_ALIAS . "_document";
+    }
+}
---
+++ Tests/Form/Type/DocumentFormTypeTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use WBW\Bundle\CoreBundle\Tests\AbstractFormTypeTestCase;
+use WBW\Bundle\EDMBundle\DependencyInjection\WBWEDMExtension;
+use WBW\Bundle\EDMBundle\Entity\Document;
+use WBW\Bundle\EDMBundle\Form\Type\DocumentFormType;
+
+/**
+ * Document form type test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Tests\Form\Type
+ */
+class DocumentFormTypeTest extends AbstractFormTypeTestCase {
+
+    /**
+     * Tests the buildForm() method.
+     *
+     * @return void
+     */
+    public function testBuildForm() {
+
+        $obj = new DocumentFormType();
+
+        $this->assertNull($obj->buildForm($this->formBuilder, [
+            "disabled" => false,
+        ]));
+
+        $this->assertCount(1, $this->childs);
+
+        $this->assertArrayHasKey("name", $this->childs);
+        $this->assertEquals(TextType::class, $this->childs["name"]["type"]);
+        $this->assertEquals("label.name", $this->childs["name"]["options"]["label"]);
+        $this->assertEquals(false, $this->childs["name"]["options"]["disabled"]);
+        $this->assertEquals(false, $this->childs["name"]["options"]["required"]);
+    }
+
+    /**
+     * Tests the configureOptions() method.
+     *
+     * @return void
+     */
+    public function testConfigureOptions() {
+
+        $obj = new DocumentFormType();
+
+        $this->assertNull($obj->configureOptions($this->resolver));
+
+        $res = [
+            "data_class"         => Document::class,
+            "translation_domain" => "WBWEDMBundle",
+        ];
+        $this->assertEquals($res, $this->defaults);
+    }
+
+    /**
+     * Tests getBlockPrefix() method.
+     *
+     * @return void
+     */
+    public function testGetBlockPrefix() {
+
+        $obj = new DocumentFormType();
+
+        $this->assertEquals(WBWEDMExtension::EXTENSION_ALIAS . "_document", $obj->getBlockPrefix());
+    }
+}
