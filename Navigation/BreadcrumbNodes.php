7929d61cded220cc4c52eb76ed3a264ef4ff5870
webeweb <support@webeweb.fr>
2020-06-14 20:15:36 +0200

    Add Breadcrumb nodes

---
+++ Navigation/BreadcrumbNodes.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Navigation;
+
+use WBW\Bundle\CoreBundle\Navigation\BreadcrumbNode;
+use WBW\Bundle\CoreBundle\Navigation\FontAwesome\BreadcrumbNodeActionEdit;
+use WBW\Bundle\CoreBundle\Navigation\FontAwesome\BreadcrumbNodeActionNew;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+
+/**
+ * Breadcrumb nodes.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Navigation
+ */
+class BreadcrumbNodes {
+
+    /**
+     * Get the EDM breadcrumb nodes with Font Awesome icons.
+     *
+     * @return BreadcrumbNode[] Returns the EDM breadcrumb nodes.
+     */
+    public static function getFontAwesomeBreadcrumbNodes() {
+
+        $nodes = [];
+
+        $nodes[] = new BreadcrumbNodeActionNew("wbw_edm_document_new", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+        $nodes[] = new BreadcrumbNodeActionEdit("wbw_edm_document_edit", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+        $nodes[] = new BreadcrumbNode("label.move", "fa:arrows-alt", "wbw_edm_document_move", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+        $nodes[] = new BreadcrumbNode("label.upload", "fa:upload", "wbw_edm_dropzone_upload", NavigationInterface::NAVIGATION_MATCHER_ROUTER);
+
+        return $nodes;
+    }
+}
---
+++ Tests/Navigation/BreadcrumbNodesTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the edm-bundle package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\EDMBundle\Tests\Navigation;
+
+use WBW\Bundle\CoreBundle\Navigation\BreadcrumbNode;
+use WBW\Bundle\CoreBundle\Navigation\NavigationInterface;
+use WBW\Bundle\EDMBundle\Navigation\BreadcrumbNodes;
+use WBW\Bundle\EDMBundle\Tests\AbstractTestCase;
+
+/**
+ * Breadcrumb nodes test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\EDMBundle\Tests\Navigation
+ */
+class BreadcrumbNodesTest extends AbstractTestCase {
+
+    /**
+     * Tests the getFontAwesomeBreadcrumbNodes() method.
+     *
+     * @return void
+     */
+    public function testGetFontAwesomeBreadcrumbNodes() {
+
+        $res = BreadcrumbNodes::getFontAwesomeBreadcrumbNodes();
+        $this->assertCount(4, $res);
+
+        $this->assertInstanceOf(BreadcrumbNode::class, $res[0]);
+        $this->assertEquals("navigation.node.action.new", $res[0]->getLabel());
+        $this->assertEquals("fa:plus", $res[0]->getIcon());
+        $this->assertEquals("wbw_edm_document_new", $res[0]->getUri());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_ROUTER, $res[0]->getMatcher());
+
+        $this->assertInstanceOf(BreadcrumbNode::class, $res[1]);
+        $this->assertEquals("navigation.node.action.edit", $res[1]->getLabel());
+        $this->assertEquals("fa:pen", $res[1]->getIcon());
+        $this->assertEquals("wbw_edm_document_edit", $res[1]->getUri());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_ROUTER, $res[1]->getMatcher());
+
+        $this->assertInstanceOf(BreadcrumbNode::class, $res[2]);
+        $this->assertEquals("label.move", $res[2]->getLabel());
+        $this->assertEquals("fa:arrows-alt", $res[2]->getIcon());
+        $this->assertEquals("wbw_edm_document_move", $res[2]->getUri());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_ROUTER, $res[2]->getMatcher());
+
+        $this->assertInstanceOf(BreadcrumbNode::class, $res[3]);
+        $this->assertEquals("label.upload", $res[3]->getLabel());
+        $this->assertEquals("fa:upload", $res[3]->getIcon());
+        $this->assertEquals("wbw_edm_dropzone_upload", $res[3]->getUri());
+        $this->assertEquals(NavigationInterface::NAVIGATION_MATCHER_ROUTER, $res[3]->getMatcher());
+    }
+}
