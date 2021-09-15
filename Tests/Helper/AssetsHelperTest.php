cd2c55886069b849427cd465df468294802cc9dd
webeweb <support@webeweb.fr>
2021-09-15 15:01:27 +0200

    Update Dropzone to 5.9.2

--- Tests/Helper/AssetsHelperTest.php
+++ Tests/Helper/AssetsHelperTest.php
@@ -51,6 +51,6 @@ class AssetsHelperTest extends AbstractTestCase {
         $res = TestAssetsHelper::listAssets($this->directoryAssets);
         $this->assertCount(1, $res);
 
-        $this->assertRegexp("/" . preg_quote("dropzone-5.8.1.zip") . "$/", $res[0]);
+        $this->assertRegexp("/" . preg_quote("dropzone-5.9.2.zip") . "$/", $res[0]);
     }
 }
