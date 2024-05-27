95b45636a17f922a9db42a9255785e8dec2af4d7
webeweb <support@webeweb.fr>
2024-05-27 11:57:25 +0200

    Update abstract command: - improve inheritance

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -11,8 +11,8 @@
 
 namespace WBW\Bundle\EDMBundle\Command;
 
-use WBW\Bundle\CoreBundle\Command\AbstractCommand as BaseCommand;
-use WBW\Bundle\CoreBundle\Translation\TranslatorTrait;
+use WBW\Bundle\CommonBundle\Command\AbstractCommand as BaseCommand;
+use WBW\Bundle\CommonBundle\Translation\TranslatorTrait;
 use WBW\Bundle\EDMBundle\WBWEDMBundle;
 
 /**
@@ -23,25 +23,4 @@ use WBW\Bundle\EDMBundle\WBWEDMBundle;
  */
 class AbstractCommand extends BaseCommand {
 
-    use TranslatorTrait {
-        setTranslator as public;
-    }
-
-    /**
-     * Translate.
-     *
-     * @param string $id The id.
-     * @param array $parameters The parameters.
-     * @param string|null $domain The domain.
-     * @param string|null $locale The locale.
-     * @return string Returns the translated id in case of success, id otherwise.
-     */
-    protected function translate(string $id, array $parameters = [], string $domain = null, string $locale = null): string {
-
-        if (null === $this->getTranslator()) {
-            return $id;
-        }
-
-        return $this->getTranslator()->trans($id, $parameters, WBWEDMBundle::getTranslationDomain(), "en");
-    }
 }
