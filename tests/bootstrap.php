3c73c42a4c2ef029c2057dda7990a5742c435e30
webeweb <support@webeweb.fr>
2024-05-28 09:52:01 +0200

    Add declare(strict_types = 1)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types = 1);
+
 if (true === file_exists($file = __DIR__ . "/autoload.php")) {
     require_once $file;
 } else if (true === file_exists($file = __DIR__ . "/autoload.php.dist")) {
